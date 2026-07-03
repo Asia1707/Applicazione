b206ef8a995125d794fad1bb7809202bd7282af6
Chiara Bertoneri <chiara.bertoneri@studenti.unipd.i>
2026-07-03 15:40:03 +0200

    settings page provvisoria

--- lib/screens/settingsscreen.dart
+++ lib/screens/settingsscreen.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // contiene il widget 
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
